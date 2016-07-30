d0b8e3fd6120a6f809d8c863859cc7bd8c124561
Edgar <etm9@jelpmi.mx>
2016-07-29 20:31:44 -0500

    NAVBAR

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'pags/pag2'
 
+  root 'pags#pag1'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
